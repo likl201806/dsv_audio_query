7f00f08b9dc00a9975fb44bf5dfd316610715ef9
李孔林 <weirongwei2016@126.com>
2025-10-20 15:04:11 +0800

    feat：增加标题打印，便于查找问题

--- android/src/main/kotlin/com/example/dsv_audio_query/DsvAudioQueryPlugin.kt
+++ android/src/main/kotlin/com/example/dsv_audio_query/DsvAudioQueryPlugin.kt
@@ -183,6 +183,7 @@ class DsvAudioQueryPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, Plug
           var finalTitle: String? = cursor.getString(titleColumn)
           var finalArtist: String? = cursor.getString(artistColumn)
           var finalAlbum: String? = cursor.getString(albumColumn)
+          Log.d(TAG, "+++ MediaStore TITLE: '${finalTitle}' (path=$filePath)")
 
           var artwork: ByteArray? = null
           var duration: Long? = null
@@ -194,6 +195,7 @@ class DsvAudioQueryPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, Plug
             val retrieverTitle = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
             val retrieverArtist = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
             val retrieverAlbum = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM)
+            Log.d(TAG, "+++ Retriever TITLE: '${retrieverTitle}' (path=$filePath)")
             
             artwork = retriever.embeddedPicture
             retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.let {
